1946b7d1daadd4b421eb9e2273526c23a4a93a79
KnightBea <88288922+KnightBea@users.noreply.github.com>
2022-09-11 12:30:18 +0530

    One two three

--- IT/thirdyear-DSA/assignment1/first.cpp
+++ IT/thirdyear-DSA/assignment1/first.cpp
@@ -4,5 +4,27 @@ using namespace std;
 
 int main(void)
 {
-    cout << "Hello, World\n";
+    cout << "Which convertion would you prefer" 
+    << endl << "1.Celcius to Fahrenheit" 
+    << endl << "2.Fahrenhiet to Celcius" << endl;
+
+    int input;
+    cin >> input;
+
+    if (input < 1 || input > 2)
+    {
+        cout << "Invalid Input!" << endl;
+        return 1;
+    }
+
+    cout << "Enter the temperature :";
+    float temp;
+    cin >> temp;
+
+    if (input == 1)
+        cout << "The temperature in Fahrenheit is : " << (temp*9/5.0) + 32  << endl;
+
+    else 
+        cout << "The temperature in Celcius is : " << (temp - 32)*(5.0/9.0) << endl;
+
 }
