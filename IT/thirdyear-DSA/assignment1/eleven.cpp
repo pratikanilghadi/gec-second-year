7ad0d74dd41849a597a7d2067ee53a728e6d8243
KnightBea <88288922+KnightBea@users.noreply.github.com>
2022-09-11 16:55:14 +0530

    Complete

--- IT/thirdyear-DSA/assignment1/eleven.cpp
+++ IT/thirdyear-DSA/assignment1/eleven.cpp
@@ -2,7 +2,56 @@
 
 using namespace std;
 
+class complex
+{
+    int real;
+    int img;
+    
+    public:
+//Constructor
+    complex(int r,int i)
+    {
+        real = r;
+        img = i;
+    }
+//Additon
+    void addition(int r,int i)
+    {
+        real += r;
+        img += i;
+    }
+//Subraction
+    void subtract(int r,int i)
+    {
+        real -= r;
+        img -= i;
+    }
+//Display
+    void display()
+    {
+        cout << "--------------------------" << endl;
+        cout << real << " + " << img << 'i' << endl;
+        cout << "--------------------------" << endl;
+    }
+};
+
 int main(void)
 {
-  cout << "Hello, World" << endl;  
+    int img,real;
+    cout << "Enter the real part of the number ";
+    cin >> real;
+    cout << endl << "Enter the imaginary part of the number ";
+    cin >> img;
+    cout << endl;
+
+    complex problem(real,img);
+    problem.display();
+    cout << endl;
+    problem.addition(10,10);
+    cout << endl;
+    problem.display();
+    cout << endl;
+    problem.subtract(5,5);
+    cout << endl;
+    problem.display();
 }
