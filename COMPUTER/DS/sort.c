61182221fb2ffc89e0a7acc669a29cf0d117b452
kalpita29naik <kalpita03naik@gmail.com>
2022-12-17 11:55:26 +0530

    complete

--- COMPUTER/DS/sort.c
+++ COMPUTER/DS/sort.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 #define MAX 100
 
 int linear(int arr[],int size,int skey);
@@ -9,6 +10,7 @@ void insertion(int arr[],int n);
 void merge_sort(int arr[],int low,int up);
 void merge(int arr[],int temp[],int low1,int up1,int low2,int up2);
 void copy(int arr[],int temp[],int low,int up);
+void print(int arr[],int n);
 
 int main()
 {
@@ -16,7 +18,8 @@ int main()
 
  while(1)
  {
-  printf("\n1-Linear Search\n");
+  printf("\n------------------------------\n");
+  printf("1-Linear Search\n");
   printf("2-Binary Search\n");
   printf("3-Bubble Sort\n");
   printf("4-Selection Sort\n");
@@ -39,13 +42,9 @@ int main()
           scanf("%d",&search);
           s=linear(arr,n,search);
           if(s!=-1)
-          {
-           printf("Element found at position %d \n",s+1);
-          }
+          { printf("Element found at position %d \n",s+1); }
           else
-          {
-           printf("Element not found\n");
-          }
+          { printf("Element not found\n"); }
           break;
 
    case 2:printf("How many elements you want to enter in the array : ");
@@ -59,13 +58,9 @@ int main()
           scanf("%d",&search);
           s=binary(arr,0,n-1,search);
           if(s!=-1)
-          {
-           printf("Element found at position %d \n",s+1);
-          }
+          { printf("Element found at position  \n"); }
           else
-          {
-           printf("Element not found\n");
-          }
+          { printf("Element not found\n"); }
           break;
 
    case 3:printf("How many elements you want to enter in the array : ");
@@ -76,10 +71,7 @@ int main()
            scanf("%d",&arr[i]);
           }
           printf("Unsorted Array is : ");
-          for(int i=0;i<n;i++)
-          {
-              printf("%d ",arr[i]);
-          }
+          print(arr,n);
           printf("\n");
           bubble(arr,n);
           break;
@@ -92,10 +84,7 @@ int main()
            scanf("%d",&arr[i]);
           }
           printf("Unsorted Array is : ");
-          for(int i=0;i<n;i++)
-          {
-              printf("%d ",arr[i]);
-          }
+          print(arr,n);
           printf("\n");
           selection(arr,n);
           break;
@@ -108,12 +97,9 @@ int main()
            scanf("%d",&arr[i]);
           }
           printf("Unsorted Array is : ");
-          for(int i=0;i<n;i++)
-          {
-              printf("%d ",arr[i]);
-          }
+          print(arr,n);
           printf("\n");
-               insertion(arr,n);
+          insertion(arr,n);
           break;
 
    case 6:printf("How many elements you want to enter in the array : ");
@@ -124,18 +110,17 @@ int main()
            scanf("%d",&arr[i]);
           }
           printf("Unsorted Array is : ");
-          for(int i=0;i<n;i++)
-          {
-              printf("%d",arr[i]);
-          }
+          print(arr,n);
           printf("\n");
           merge_sort(arr,0,n-1);
           printf("\nSorted array : ");
-          for(int i=0;i<n;i++)
-          {
-           printf("%d ",arr[i]);
-          }
+          print(arr,n);
           break;
+
+   case 7:exit(1);
+          break;
+
+   default:printf("Invalid choice\n");
   }
  }
 }
@@ -144,39 +129,25 @@ int linear(int arr[],int size,int skey)
 {
  int i=0;
  while(i<size && skey!=arr[i])
- {
-  i++;
- }
+ { i++; }
  if(i<size)
- {
-  return i;
- }
+ { return i; }
  else
- {
-  return -1;
- }
+ { return -1; }
 }
 
 int binary(int arr[],int low,int up,int skey)
 {
        int mid;
        if(low>up)
-       {
-              return -1;
-       }
+       { return -1; }
        mid=(low+up)/2;
        if(skey<arr[mid])
-       {
-              binary(arr,low,mid,skey);
-       }
+       { binary(arr,low,mid,skey); }
        else if(skey>arr[mid])
-       {
-              binary(arr,mid+1,up,skey);
-       }
+       {  binary(arr,mid+1,up,skey); }
        else
-       {
-              return mid;
-       }
+       { return mid; }
        return 0;
 }
 
@@ -188,10 +159,7 @@ void selection(int arr[],int n)
               for(int j=i+1;j<n;j++)
               {
                      if(arr[min]>arr[j])
-                     {
-                            min=j;
-                     }
-                     
+                     {  min=j; }    
               }
               if(i!=min)
               {
@@ -199,21 +167,11 @@ void selection(int arr[],int n)
                      arr[i]=arr[min];
                      arr[min]=temp;
               }
-                     printf("Pass %d :",i+1);
-                     for(int i=0;i<n;i++)
-                     {
-                            printf("%d ",arr[i]);
-                     }
-                     printf("\n");
-
-
+              printf("Pass %d :",i+1);
+              print(arr,n);
        }
        printf("Sorted array : ");
-       for(int i=0;i<n;i++)
-       {
-              printf("%d ",arr[i]);
-       }
-       printf("\n");
+       print(arr,n);
 }
 
 void bubble(int arr[],int n)
@@ -234,24 +192,12 @@ void bubble(int arr[],int n)
                      
               }
               if(change==0)
-              {
-                     break;
-              }
-               printf("Pass %d :",i+1);
-                     for(int i=0;i<n;i++)
-                     {
-                            printf("%d ",arr[i]);
-                     }
-                     printf("\n");
-              
+              { break; }
+              printf("Pass %d :",i+1);
+              print(arr,n); 
        }
        printf("Sorted array : ");
-       for(int i=0;i<n;i++)
-       {
-              printf("%d ",arr[i]);
-       }
-       printf("\n");
-
+       print(arr,n);
 }
 
 void insertion(int arr[],int n)
@@ -261,23 +207,13 @@ void insertion(int arr[],int n)
        {
               k=arr[i];
               for(j=i-1;j>=0 && k<arr[j];j--)
-              {
-                     arr[j+1]=arr[j];
-              }
+              { arr[j+1]=arr[j]; }
               arr[j+1]=k;
               printf("Pass %d: ",i);
-              for(int t=0;t<n;t++)
-              {
-                     printf("%d ",arr[t]);
-              }
-              printf("\n");
+              print(arr,n);
        }
        printf("Sorted array : ");
-       for(int i=0;i<n;i++)
-       {
-              printf("%d ",arr[i]);
-       }
-       printf("\n");
+       print(arr,n);
 }
 
 void merge_sort(int arr[],int low,int up)
@@ -300,22 +236,14 @@ void merge(int arr[],int temp[],int low1,int up1,int low2,int up2)
        while((i<=up1) && (j<=up2))
        {
               if(arr[i]<= arr[j])
-              {
-                     temp[k++]=arr[i++];
-              }
+              { temp[k++]=arr[i++]; }
               else
-              {
-                     temp[k++]=arr[j++];
-              }
+              { temp[k++]=arr[j++]; }
        }
        while(i<=up1)
-       {
-              temp[k++]=arr[i++];
-       }
+       { temp[k++]=arr[i++]; }
        while(j<=up2)
-       {
-              temp[k++]=arr[j++];
-       }  
+       { temp[k++]=arr[j++]; }  
        
 }
 
@@ -323,7 +251,12 @@ void copy(int arr[],int temp[],int low,int up)
 {
        int i;
        for(int i=low;i<=up;i++)
-       {
-              arr[i]=temp[i];
-       }
+       { arr[i]=temp[i]; }
+}
+
+void print(int arr[],int n)
+{
+       for(int i=0;i<n;i++)
+       { printf("%d ",arr[i]); }
+       printf("\n");
 }
