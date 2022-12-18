395f8bf2fd4953acaa7154c7418293d7a541d5e8
kalpita29naik <kalpita03naik@gmail.com>
2022-12-18 09:36:07 +0530

    complete

---
+++ COMPUTER/OOPS/TW/4c.cpp
@@ -0,0 +1,37 @@
+/*Write a c++ program to explain the concept of stack unwinding incase of exception handling*/
+
+#include<iostream>
+
+using namespace std;
+
+void fun1()
+{
+ cout<<"Inside fun1()"<<endl;
+ throw 1;
+}
+
+void fun2()
+{
+ cout<<"Inside fun2()"<<endl;
+ fun1();
+}
+
+void fun3()
+{
+ cout<<"Inside fun3()"<<endl;
+ fun2();
+}
+
+int main()
+{
+ try
+ {
+  cout<<"Inside try block"<<endl;
+  fun3();
+ }
+ catch(int i)
+ {
+  cout<<"Inside catch block"<<endl;
+ }
+ return 0;
+}
